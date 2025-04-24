783522e6d29153f321ba456815c82919414a9d0e
martta <martineidger@gmail.com>
2025-04-24 18:04:51 +0300

    add recipeService

---
+++ RecipesService/Recipes.API/KafkaTests/DailyRecipe.cs
@@ -0,0 +1,8 @@
+﻿namespace Recipes.API.KafkaTests
+{
+    public class DailyRecipe
+    {
+        public string Id {  get; set; } = Guid.NewGuid().ToString();
+        public string Name { get; set; }
+    }
+}
---
+++ RecipesService/Recipes.API/Program.cs
@@ -0,0 +1,39 @@
+using Messaging.Kafka.Producing;
+using Messaging.Kafka.Producing.Abstractions;
+using Microsoft.AspNetCore.Http.HttpResults;
+using Recipes.API.GrpcTests;
+using Recipes.API.KafkaTests;
+
+var builder = WebApplication.CreateBuilder(args);
+
+builder.Services.AddSingleton<UserInfoService>();
+builder.Services.AddGrpcClient<UserService.Protos.UserService.UserServiceClient>(options =>
+{
+    options.Address = new Uri(builder.Configuration["UserServiceUrl"]);
+})
+.ConfigurePrimaryHttpMessageHandler(() =>
+{
+    var handler = new HttpClientHandler();
+    handler.ServerCertificateCustomValidationCallback =
+        HttpClientHandler.DangerousAcceptAnyServerCertificateValidator;
+    return handler;
+});
+
+builder.Services.AddProducer<DailyRecipe>(builder.Configuration.GetSection("Kafka:DailyRecipe"));
+
+var app = builder.Build();
+
+app.MapGet("/get-user", async (UserInfoService userInfoService, string id) =>
+{
+    var info = await userInfoService.GetUserData(id);
+    return Results.Ok(info);
+});
+
+app.MapGet("/daily-recipe", (IKafkaProducer<DailyRecipe> producer) => {
+    producer.ProduceAsync(new DailyRecipe()
+    {
+        Name = "Test",
+    }, default);
+});
+
+app.Run();
